a30c90a125a20c4f5d4517402c0a1853c3af0b5b
jeffsantaines <jeffsantaines@gmail.com>
2017-12-04 22:01:22 +1300

    BottomNavigationView added in the XML

--- app/src/main/java/com/example/jeffsantaines/jeffsmusicapp/BrowseFragment.java
+++ app/src/main/java/com/example/jeffsantaines/jeffsmusicapp/BrowseFragment.java
@@ -2,6 +2,7 @@ package com.example.jeffsantaines.jeffsmusicapp;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,6 +19,11 @@ public class BrowseFragment extends Fragment {
         // Required empty public constructor
     }
 
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
--- app/src/main/java/com/example/jeffsantaines/jeffsmusicapp/LibraryFragment.java
+++ app/src/main/java/com/example/jeffsantaines/jeffsmusicapp/LibraryFragment.java
@@ -18,6 +18,10 @@ public class LibraryFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
